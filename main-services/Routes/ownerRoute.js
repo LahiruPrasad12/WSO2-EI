1ed6761957cc01fda7fbcd2c238c82fab2aef7d4
kavindushehan <it20076566@my.sliit.lk>
2022-05-16 00:30:41 +0530

    Finished add to cart and item view page

--- main-services/Routes/ownerRoute.js
+++ main-services/Routes/ownerRoute.js
@@ -11,7 +11,7 @@ router.route('/')
 
 //This api-resource route for update and delete specific student
 router.route('/:id')
-  .get(authController.protect, authController.restrictTo('owner'), productController.findProduct)
+  .get(authController.protect, productController.findProduct)
   .patch(authController.protect, authController.restrictTo('owner'), productController.updateProduct)
   .delete(authController.protect, authController.restrictTo('owner'), productController.deleteProduct)
 
--- ui/src/apis/modules/owner.js
+++ ui/src/apis/modules/owner.js
@@ -4,4 +4,5 @@ const resource = '/api/owner';
 export default {
     listPrduct: () => api.get(`${resource}`),
     createProduct: (payload) => api.post(`${resource}`, payload),
+    getProduct: (id) => api.get(`${resource}/${id}`),
 }
