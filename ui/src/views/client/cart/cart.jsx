62c95ae1a04e183ee6fa38ed4171b0f25c5cc458
amilasm <amilasampath1400@gmail.com>
2022-05-19 13:38:29 +0530

    payment and filters add

--- ui/src/views/client/cart/cart.jsx
+++ ui/src/views/client/cart/cart.jsx
@@ -116,7 +116,7 @@ const Cart = () => {
                             <hr />
                             <h3 class="h6 pt-4 font-weight-semibold"><span class="badge badge-success mr-2">Note</span>Additional Comments</h3>
                             <textarea class="form-control mb-3" id="order-comments" rows="5"></textarea>
-                            <Link to="/cardpay"><a class="btn btn-primary btn-block" href="#">
+                            <Link to="/payment"><a class="btn btn-primary btn-block" href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card mr-2">
                                     <rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect>
                                     <line x1="1" y1="10" x2="23" y2="10"></line>
--- ui/src/views/client/home/home.jsx
+++ ui/src/views/client/home/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './chome.css'
+import './search-filter.css'
 import { Link } from 'react-router-dom';
 import Footer from '../../../layouts/footer';
 import Header from '../../../layouts/header';
@@ -26,12 +27,27 @@ const ClientHome = () => {
         <div>
             <Header />
             <img src="https://i.postimg.cc/BbrzhpXf/services-left-dec.png" alt="" class="shape" />
-
             <div class="container" >
                 <div className='shopc'>
                     <h1>SHOP THE COLLECTION</h1>
                     <p><Link to="/">Home</Link> / Products</p>
+                    <br/>
+                    <form action="#" method="get" id="searchForm" class="input-group">
+                        <input type="text" class="form-control" name="x" placeholder="Search Products..."/>
+                            <div class="input-group-btn search-panel">
+                                <select name="search_param" id="search_param" style={{borderRadius: '0px', width: '100px'}} class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+                                    <option value="all">All</option>
+                                    <option value="username">Username</option>
+                                    <option value="email">Email</option>
+                                    <option value="studentcode">Student Code</option>
+                                </select>
+                            </div>
+                            <button type="button" style={{borderRadius: '0px'}} class="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>
+                            </button>
+                    </form>    
                 </div>
+                
                 <div className='c2'>
                     <div class='rowp'>
                         {products.map((item, index) => {
